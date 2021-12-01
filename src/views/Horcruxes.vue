e6a7e3202d58c91ae047a63be964de2e0dd79a80
julianaizac <dev.julianaizac@outlook.com>
2021-11-30 23:19:47 -0300

    card e view para horcruxes

---
+++ src/views/Horcruxes.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col v-for="(data, i) in cards" 
+                    :key="i" cols="12" sm="12" md="4" xs="2">
+                <CardHorcrux :cardInfo="data"/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+
+import CardHorcrux from "../components/CardHorcrux.vue"
+
+export default {
+    name: 'Horcruxes',
+
+    components: {
+        CardHorcrux
+    },
+
+    computed:{
+        cards(){
+            return this.$store.state.cardsHorcruxes
+        }
+    }
+}
+
+
+</script>
