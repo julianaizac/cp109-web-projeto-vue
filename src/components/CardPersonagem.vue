e8ba61c36246747a2f298b6b24c8f1b0e421b1de
julianaizac <dev.julianaizac@outlook.com>
2021-11-30 23:02:41 -0300

    card e view para personagens

---
+++ src/components/CardPersonagem.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card class="mx-auto" max-width="350" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          {{cardInfo.casaDeHogwarts}}
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          {{cardInfo.nome}}
+        </v-list-item-title>
+        <v-list-item-subtitle>{{cardInfo.nascimento}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar size="80">
+        <v-img :src="require(`@/assets/personagens/${cardInfo.urlImagem}`)"></v-img>
+      </v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+
+export default{
+    props:['cardInfo']
+}
+
+</script>
---
+++ src/views/Personagens.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col v-for="(data, i) in cards" 
+                    :key="i" cols="12" sm="12" md="4" xs="2">
+                <CardPersonagem :cardInfo="data"/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+
+import CardPersonagem from "../components/CardPersonagem.vue"
+
+export default {
+    name: 'Personagens',
+
+    components: {
+        CardPersonagem
+    },
+
+    computed:{
+        cards(){
+            return this.$store.state.cardsPersonagens
+        }
+    }
+}
+
+</script>
