c43e79d49ff729a8bafc8996a0e19d533731f788
julianaizac <dev.julianaizac@outlook.com>
2021-11-30 22:20:16 -0300

    criação da view e card para as casas de hogwarts

---
+++ src/views/Casas.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col v-for="(data, i) in cards" :key="i">
+                <CardCasas :cardInfo="data"/>
+            </v-col>
+        </v-row>
+    </v-container>
+    
+</template>
+
+<script>
+
+import CardCasas from "../components/CardCasas.vue"
+
+export default {
+    name: 'Casas',
+
+    components: {
+        CardCasas
+    },
+
+    computed:{
+        cards(){
+            return this.$store.state.cardsCasas
+        }
+    }
+}
+
+
+</script>
